cee8bb7fccc105f9f17d02ff92d6aa28624c9201
Li Yu <gachuka@gmail.com>
2022-09-21 11:03:05 -0400

    Fix data character_left number bouncing in front end issue

--- server.js
+++ server.js
@@ -15,13 +15,40 @@ function readSaveFile() {
   return saveData;
 }
 
+// function mapSaveFile(saveData, data) {
+//   const newData = {}
+//   for (const [key, value] of Object.entries(saveData)) {
+//     if (key === "text_typed" && data.text_typed) {
+//       newData[key] = (saveData.text_typed + data.text_typed).slice(-110)
+//     } else if (key === 'character_count' && data.character_count) {
+//       newData[key] = data.character_count 
+//     } else if (key === 'character_left') {
+//       newData[key] = saveData.character_count - (saveData.upgrade_1 * 5000) - (saveData.upgrade_2 * 10000) - (saveData.upgrade_3 * 5000)
+//     } else if (key === 'upgrade_1' && data.upgrade_1) {
+//       newData[key] = data.upgrade_1
+//     } else if (key === 'upgrade_2' && data.upgrade_2) {
+//       newData[key] = data.upgrade_2
+//     } else if (key === 'upgrade_3' && data.upgrade_3) {
+//       newData[key] = data.upgrade_3
+//     } else if (key === 'add_per_input') {
+//       newData[key] = (1 + saveData.upgrade_1) * (2 ** saveData.upgrade_2 )
+//     } else if (key === 'timestamp') {
+//       newData[key] = Date.now()
+//     } else {
+//       newData[key] = value
+//     }
+//   }
+//   return newData
+// }
 function mapSaveFile(saveData, data) {
   const newData = {}
   for (const [key, value] of Object.entries(saveData)) {
     if (key === "text_typed" && data.text_typed) {
       newData[key] = (saveData.text_typed + data.text_typed).slice(-110)
     } else if (key === 'character_count' && data.character_count) {
-      newData[key] = data.character_count
+      newData[key] = data.character_count 
+    } else if (key === 'character_left') {
+      newData[key] = (data.character_count ? data.character_count : saveData.character_count) - ((data.upgrade_1 ? data.upgrade_1: saveData.upgrade_1) * 500) - ((data.upgrade_2 ? data.upgrade_2 : saveData.upgrade_2) * 1000) - ((data.upgrade_3 ? data.upgrade_3 : saveData.upgrade_3) * 750)
     } else if (key === 'upgrade_1' && data.upgrade_1) {
       newData[key] = data.upgrade_1
     } else if (key === 'upgrade_2' && data.upgrade_2) {
@@ -29,7 +56,7 @@ function mapSaveFile(saveData, data) {
     } else if (key === 'upgrade_3' && data.upgrade_3) {
       newData[key] = data.upgrade_3
     } else if (key === 'add_per_input') {
-      newData[key] = (1 + saveData.upgrade_1) * (saveData.upgrade_2 === 0 ? 1 : saveData.upgrade_2 * saveData.upgrade_2 )
+      newData[key] = (1 + (data.upgrade_1 ? data.upgrade_1: saveData.upgrade_1)) * (2 ** (data.upgrade_2 ? data.upgrade_2 : saveData.upgrade_2) )
     } else if (key === 'timestamp') {
       newData[key] = Date.now()
     } else {
@@ -50,10 +77,11 @@ app.put('/', (req, res) => {
   const saveFileData = readSaveFile()
   const reqBody = req.body
   const newSaveFileData = mapSaveFile(saveFileData, reqBody)
-  console.log('Game Saved', newSaveFileData)
-
+  
   fs.writeFileSync("./data/save_file.json", JSON.stringify(newSaveFileData))
-  res.status(200).json(req.body.text_typed)
+  const readAgain = readSaveFile()
+  console.log('Game Saved', readAgain)
+  res.status(200).json(req.body)
 })
 
 app.listen(PORT, () => {
