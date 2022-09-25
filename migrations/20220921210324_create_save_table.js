4bf00d290f8bccb6ab2cb38073d00efdf2f13cee
Li Yu <gachuka@gmail.com>
2022-09-25 10:14:04 -0400

    Change column type

--- migrations/20220921210324_create_save_table.js
+++ migrations/20220921210324_create_save_table.js
@@ -2,13 +2,13 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('save', (table) => {
       table.increments('id').primary();
-      table.string('text_typed').notNullable().defaultTo('');
-      table.string('character_count').notNullable().defaultTo(0);
-      table.string('character_left').notNullable().defaultTo(0);
+      table.string('text_typed').notNullable().defaultTo(' ');
+      table.bigint('character_count').notNullable().defaultTo(0);
+      table.bigint('character_left').notNullable().defaultTo(0);
       table.string('upgrade_1').notNullable().defaultTo(0);
       table.string('upgrade_2').notNullable().defaultTo(0);
       table.string('upgrade_3').notNullable().defaultTo(0);
-      table.string('add_per_input').notNullable().defaultTo(1);
+      table.bigint('add_per_input').notNullable().defaultTo(1);
       table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
 };
