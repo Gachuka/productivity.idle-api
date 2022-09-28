1b4151deff4b49e50df2329c1d829354e37442df
Li Yu <gachuka@gmail.com>
2022-09-28 14:30:16 -0400

    Update migrate table

--- migrations/20220921210324_create_save_table.js
+++ migrations/20220921210324_create_save_table.js
@@ -5,9 +5,9 @@ exports.up = function (knex) {
       table.string('text_typed').notNullable().defaultTo(' ');
       table.bigint('character_count').notNullable().defaultTo(0);
       table.bigint('character_left').notNullable().defaultTo(0);
-      table.int('upgrade_1').notNullable().defaultTo(0);
-      table.int('upgrade_2').notNullable().defaultTo(0);
-      table.int('upgrade_3').notNullable().defaultTo(0);
+      table.integer('upgrade_1').notNullable().defaultTo(0);
+      table.integer('upgrade_2').notNullable().defaultTo(0);
+      table.integer('upgrade_3').notNullable().defaultTo(0);
       table.bigint('add_per_input').notNullable().defaultTo(1);
       table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
